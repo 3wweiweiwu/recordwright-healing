a018e4083a8ce3f4b8cc823b7927a113bc62af8c
VELASQUD <daniel.velasquez@aspentech.com>
2023-08-24 14:01:55 -0700

    changes ut

--- test/controller/snapshot/spec.snapshot.js
+++ test/controller/snapshot/spec.snapshot.js
@@ -1,13 +1,25 @@
-const { assert } = require('console')
+//const { assert } = require('console')
+import assert from 'node:assert';
 const { HtmlSnapshotCompresed } = require('../../../controller/snapshot')
 const fs = require('fs')
 const path = require('path')
 beforeEach('Read and return the Snapshot of the web page in HtmlSnapshotCompresed', async() => {
     let snapshotJson = await require('./files/snapshot-test-1.json')
-    return new HtmlSnapshotCompresed(JSON.stringify(snapshotJson))
+    let snapshotJsonNode = await require('./files/snapshot-test-1.json')
+    let compressionComplete = new HtmlSnapshotCompresed(JSON.stringify(snapshotJson))
+    let baselineComplete = compressionComplete.parse(snapshotJson)
+    let compressionNode = new HtmlSnapshotCompresed(JSON.stringify(snapshotJsonNode))
+    let baselineNode = compressionNode.parse(snapshotJsonNode)
 })
 
 describe('compressed snapshot', () => {
+    describe('Test function by function', () =>{
+        it('Test updateTextInCurrnetLevel function', async () =>{
+            baselineNode[0][0].text = '\n\t\s'
+            let newNode = compressionNode.updateTextInCurrnetLevel(baselineNode[0])
+            assert.doesNotMatch(newNode[0].text, /\n\s+/g, `text match with /\\n\\s+/g`)
+        })
+    })
     it('should compress snapshot with happy path', async () => {
         
     }).timeout(1000000)
